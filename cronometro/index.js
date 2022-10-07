8c48dff83c08a71800a367db99e31a67dc1d8e05
Fernando Alves <lfalvespe@gmail.com>
2022-10-07 10:43:20 -0300

    Update index.js

--- cronometro/index.js
+++ cronometro/index.js
@@ -1,15 +1,24 @@
 hora = document.querySelector('#hora')
 minuto = document.querySelector('#minuto')
 segundo = document.querySelector('#segundo')
+milisegundo = document.querySelector('#milisegundo')
 
 s = 0
 m = 0
 h = 0
+ms = 0
 
 function iniciar(){
 
     time = setInterval(function() {
-        s += 1
+        ms += 1
+
+        if (ms < 100) {
+            milisegundo.innerHTML = (ms < 10 ? '0' + ms : ms)
+        } else {
+            ms = 0
+            s += 1
+        }
 
         if (s < 60) {
             segundo.innerHTML = (s < 10 ? '0' + s : s)
@@ -28,7 +37,7 @@ function iniciar(){
         
         }
 
-    }, 1000)
+    }, 10)
 
 }
 
@@ -41,8 +50,10 @@ function zerar() {
     s = 0
     m = 0
     h = 0
+    ms = 0
 
     segundo.innerHTML = '00'
     minuto.innerHTML = '00'
     hora.innerHTML = '00'
+    milisegundo.innerHTML = '00'
 }
